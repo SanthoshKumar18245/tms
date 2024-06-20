245b000379a0d8519c6c467593974fb0c14a40ac
Prassadraj <prassadpr10@gmail.com>
2024-06-20 15:55:40 +0530

    chg

--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -5,15 +5,21 @@ import {
   faHouse,
   faListCheck,
 } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Side() {
+  const location = useLocation();
+
   return (
     <div className="relative top-0">
       <nav className="w-[17vw] h-full bg-bgBlack border-r-bgGray border-b-bgGray border-t-bgGray border-r-[1px] border-t-[1px] border-b-[1px] ">
         <div className="text-name mt-14 font-poppins flex flex-col justify-center items-center gap-6">
           <Link to="/dashboard">
-            <div className="flex w-[15vw] rounded-lg p-3 items-center justify-center">
+            <div
+              className={`flex w-[15vw] rounded-lg p-3 items-center justify-center ${
+                location.pathname == "/dashboard" ? "bg-bgGray" : ""
+              }`}
+            >
               <div className="flex justify-center items-center mr-7">
                 <FontAwesomeIcon icon={faHouse} className="text-xl" />
               </div>
@@ -21,7 +27,11 @@ function Side() {
             </div>
           </Link>
           <Link to="/ticket">
-            <div className="flex w-[15vw] bg-bgGray p-3 items-center justify-center rounded-lg">
+            <div
+              className={`flex w-[15vw] p-3 items-center justify-center rounded-lg ${
+                location.pathname == "/ticket" ? "bg-bgGray" : ""
+              }`}
+            >
               <div className="flex justify-center items-center mr-7">
                 <FontAwesomeIcon icon={faListCheck} className="text-xl" />
               </div>
@@ -29,7 +39,11 @@ function Side() {
             </div>
           </Link>
           <Link to="/analytics">
-            <div className="flex w-[15vw] p-3 items-center justify-center rounded-lg">
+            <div
+              className={`flex w-[15vw] p-3 items-center justify-center rounded-lg ${
+                location.pathname == "/analytics" ? "bg-bgGray" : ""
+              }`}
+            >
               <div className="flex justify-center items-center mr-8">
                 <FontAwesomeIcon icon={faChartSimple} className="text-xl" />
               </div>
